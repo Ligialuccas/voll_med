502ffb7aa545f854f7d70e735205526f62f40ee6
Ligialuccas <ligia.aluccas@gmail.com>
2025-08-01 11:09:10 -0300

    Implementado agendamento/cancelamento de consultas, regras de negócio

--- src/main/java/med/voll/api/domain/paciente/PacienteRepository.java
+++ src/main/java/med/voll/api/domain/paciente/PacienteRepository.java
@@ -1,10 +1,18 @@
 package med.voll.api.domain.paciente;
 
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.NativeQuery;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
 
     Page<Paciente> findAllByAtivoTrue(Pageable paginacao);
+
+
+    @Query(value = "SELECT p.ativo FROM pacientes p WHERE p.id = :id" ,nativeQuery = true)
+    Boolean findAtivoById(Long id);
 }
